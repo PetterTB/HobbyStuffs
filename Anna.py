4e078e60745f658e6f3346250539800e132d3568
me <petter.bossum@gmail.com>
2017-11-12 13:14:53 +0100

    Implemented arguments to Anna.py

--- Anna.py
+++ Anna.py
@@ -6,17 +6,29 @@ from GameInput import GameInput
 import argparse
 
 
-def screenGrab():
-    #box = (157, 346, 796, 825)
-    box = ()
-    im = ImageGrab.grab(box)
-    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
-            '.png', 'PNG')
+def screen_grab(path):
+
+    i = 0
+    while(True)
+        box = ()
+        im = ImageGrab.grab(box)
+        save = path + str(i) + ".png"
+        im.save(save, 'PNG')
+        time.sleep(3)
+        i += 1
 
 def parse_args():
-    pass
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--record_to', default = None,
+                        help = "Recore and store to location.")
+    parser.add_argument('--hunt', default = None, help = "Which hunting style?")
 
-def main():
+    args = parser.parse_args()
+    return args
+
+def hunt_wasps():
+
     while(True):
 
         c = CaveHunter(GameInput())
@@ -28,6 +40,14 @@ def main():
         c.go_up_with_rope()
         c.hunt_floor(50)
         c.go_up_with_rope()
+
+
+def main():
+
+    args = parse_args()
+    if args.hunt == "wasp":
+        hunt_wasps()
+
 
 
 if __name__ == '__main__':
